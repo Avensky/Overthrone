72f3a054b9553f95ec77971fcd0019d57e4bff7e
uriel zacarias <urielzacarias@gmail.com>
2020-05-26 01:13:56 -0700

    Added sample character component

---
+++ client/src/components/pages/Characters/Charcter/Character.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from '../../Pages.module.css'
+import myImg from '../../../../assets/images/character.jpg';
+
+const character = props => (
+    <div className={classes.Card}>
+        <figure className={classes.CardThumbnail}>
+            <img src={myImg} alt="character"/>
+        </figure>
+        <div className={classes.CardTitle}>
+            {props.title}
+        </div>
+        <div className={classes.CardDescription}>{props.content}</div>
+        <div className={classes.CardDetails}>
+            By {props.author}
+            on {props.published}
+        </div>
+    </div>
+)
+
+export default character;
