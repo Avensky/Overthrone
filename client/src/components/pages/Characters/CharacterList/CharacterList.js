be55ff3cf8ed7c3b11f631f83f87afe801a4c196
uriel zacarias <urielzacarias@gmail.com>
2020-07-19 23:37:38 -0700

    fixed loading error from sorting alpha

--- client/src/components/pages/Characters/CharacterList/CharacterList.js
+++ client/src/components/pages/Characters/CharacterList/CharacterList.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import Auxiliary from '../../../../hoc/Auxiliary';
 import Character from '../Character/Character';
 import myClasses from './CharacterList.module.scss';
-//import classes from '../../Pages.module.scss';
+import classes from '../../Pages.module.scss';
 import NewCharacter from '../NewCharacter/NewCharacter';
 import * as actions from '../../../../store/actions/index';
 //import CharacterEdit from './CharacterEdit/CharacterEdit';
@@ -19,22 +19,10 @@ class Characters extends Component {
 
     componentDidMount() {
         console.log(this.props)
-        
-        const characters = this.props.chars;
-        characters.sort(function(a, b) {
-            var textA = a.name.toUpperCase();
-            var textB = b.name.toUpperCase();
-            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        });
+    }
 
-        const updatedChars = characters.map( char => {
-            return {
-                ...char,
-            }
-        });
-        this.setState({ characters: updatedChars})
+    componentDidUpdate() {
 
-        console.log('characters: ' + characters)
     }
 
     deleteCharHandler = (id) => {
@@ -42,10 +30,18 @@ class Characters extends Component {
     }
 
     render () {
-        let chars = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+        let characters = <p style={{textAlign: 'center'}}>Something went wrong!</p>
 
         if (!this.props.error) {
-            chars = this.state.characters.map( char => {
+            let chars = this.props.chars
+
+            chars.sort(function(a, b){
+                if(a.name < b.name) { return -1; }
+                if(a.name > b.name) { return 1; }
+                return 0;
+            })
+
+            characters = chars.map( char => {
                 return (
                     <Character
                         key         = {char._id}  
@@ -65,8 +61,19 @@ class Characters extends Component {
                 <div className={myClasses.Items}>
                     <div className={['box', myClasses.Items ].join(' ')}></div>
                     <NewCharacter />
-                    <Search/>
-                    {chars}
+                    <div className={classes.spread}>
+                    <Search className={myClasses.Search} />
+                    <div className={myClasses.dropdown}>
+                        <button className={myClasses.dropbtn}>OrderBy: </button>
+                        <div className={myClasses.dropdownContent}>
+                            <a href="/price">Alphabetical</a>
+                            <a href="/price">Price</a>
+                            <a href="/date">Most recent</a>
+                            <a href="/popular">Most Popular</a>
+                        </div>
+                    </div>
+                </div>
+                    {characters}
                 </div>
 
             </Auxiliary>
