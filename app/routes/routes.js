aed9aaa19eabbe894b256284d190cfd6823bb155
uriel zacarias <urielzacarias@gmail.com>
2020-07-22 18:23:06 -0700

    removed / route from server

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,9 +4,9 @@ module.exports = function(app, passport) {
 // normal routes ===============================================================
 // =============================================================================
 	// show the home page (will also have our login links)
-	app.get('/', function(req, res) {
-		res.render('index.ejs');
-	});
+//	app.get('/', function(req, res) {
+//		res.render('index.ejs');
+//	});
 
 	app.get('/api/fetchUser', (req, res) => {
         if (req.user){
