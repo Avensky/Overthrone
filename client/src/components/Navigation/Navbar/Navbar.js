9f03605691a6dc769cf72641fe3c0487222be130
uriel zacarias <urielzacarias@gmail.com>
2020-07-23 23:47:15 -0700

    cart on nav bar only shows when there are items in the cart

--- client/src/components/Navigation/Navbar/Navbar.js
+++ client/src/components/Navigation/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { connect } from 'react-redux';
 import classes from './Navbar.module.scss';
 import Logo from '../../UI/Logo/Logo';
@@ -6,31 +6,41 @@ import NavItems from '../NavItems/NavItems';
 import SidebarToggle from '../Sidebar/SidebarToggle/SidebarToggle';
 import { NavLink } from 'react-router-dom';
 
-const navbar = ( props ) => (
-    <div className={classes.Navbar}>
-        <SidebarToggle clicked={props.sidebarToggleClicked} />   
-        <div className={[classes.MobileLinks, classes.Mobile].join(' ')}>
-            <h2>            
-                <NavLink  to="/shop/cart">
-                     <span className={["fa", "fa-shopping-cart", classes.left].join(' ')}/> {/*  ({props.items.length})  */}
-                </NavLink >
-                <NavLink  to="/authentication">
-                    <span className={["fa", classes.fa, "fa-user", classes.left].join(' ')}/> 
-                </NavLink >
-            </h2>
-            <div className={[classes.Logo, classes.Mobile].join(' ')}>
-            <NavLink  to="/">
-                <div className={classes.Logo}>
-                    <Logo />
-                </div>  
-            </NavLink >
-        </div>
-        </div>
-        <div className={classes.DesktopOnly}>
-            <NavItems isAuthenticated={props.isAuth} />
-        </div>
-    </div>
-);
+class navbar extends Component {
+    render () {
+        let cart;
+            if (this.props.items.length>0) {
+                cart = (
+                    <NavLink  to="/shop/cart">
+                        <span className={["fa", "fa-shopping-cart", classes.left].join(' ')}/>
+                    </NavLink >
+                )
+            }
+        return (
+            <div className={classes.Navbar}>
+                <SidebarToggle clicked={this.props.sidebarToggleClicked} />   
+                <div className={[classes.MobileLinks, classes.Mobile].join(' ')}>
+                    <h2>          
+                        {cart}
+                        <NavLink  to="/authentication">
+                            <span className={["fa", classes.fa, "fa-user", classes.left].join(' ')}/> 
+                        </NavLink >
+                    </h2>
+                    <div className={[classes.Logo, classes.Mobile].join(' ')}>
+                    <NavLink  to="/">
+                        <div className={classes.Logo}>
+                            <Logo />
+                        </div>  
+                    </NavLink >
+                </div>
+                </div>
+                <div className={classes.DesktopOnly}>
+                    <NavItems isAuthenticated={this.props.isAuth} />
+                </div>
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = state => {
     return {
