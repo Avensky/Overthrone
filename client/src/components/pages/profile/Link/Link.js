c3aac812b3471aed9eb1e3d800c5f02f745bf5ae
uriel zacarias <urielzacarias@gmail.com>
2020-08-18 12:13:18 -0700

    made profile pagemore dynamic

--- client/src/components/pages/profile/Link/Link.js
+++ client/src/components/pages/profile/Link/Link.js
@@ -11,13 +11,13 @@ const link = (props) => (
                 <span> {props.link}</span>
             </h3>
             <p>
-                <strong>{props.id}</strong>: <br />
-                <strong>{props.email}</strong>: <br />
-                <strong>{props.token}</strong>: <br />
-                <strong>{props.name}</strong>: <br />
-                <strong>{props.displayName}</strong>: <br />
-                <strong>{props.username}</strong>: <br />
-                <strong>{props.password}</strong>:
+                { props.name        ? <strong>Name:          {props.name}<br /></strong>         : null }
+                { props.displayName ? <strong>Display Name:   {props.displayName}<br /></strong>  : null }
+                { props.username    ? <strong>Username:      {props.username}<br /></strong>     : null }
+                { props.id          ? <strong>Id:            {props.id}<br /></strong>           : null }
+                { props.email       ? <strong>Email:         {props.email}<br /></strong>        : null }
+                { props.token       ? <strong>Token:         {props.token}<br /></strong>        : null }
+                { props.password    ? <strong>Password:      {props.password}<br /></strong>     : null }
             </p>
             <a href="/unlink/local" className="btn btn-default">Unlink</a>
             <a href="/connect/local" className={["btn", props.mystyle].join(' ')}>Connect {props.link}</a>
