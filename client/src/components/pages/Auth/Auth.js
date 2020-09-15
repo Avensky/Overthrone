b95941e6eb156f751e0a3fd1eb52cdea5a5f6d9a
uriel zacarias <urielzacarias@gmail.com>
2020-09-14 18:09:26 -0700

    setting up indicative validation

--- client/src/components/pages/Auth/Auth.js
+++ client/src/components/pages/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { validate } from 'indicative/validator';
+import { sanitize } from 'indicative/sanitizer'
 import {connect} from 'react-redux';
 import classes from '../Pages.module.scss';
 import myClasses from './Auth.module.scss';
@@ -13,54 +15,103 @@ import { Redirect } from 'react-router-dom';
 
 class Auth extends Component {
     state = {
-        controls: {
-            email: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    name: 'email',
-                    placeholder: 'Email Address'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    isEmail: true
-                },
-                valid: false,
-                touched: false
+        email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                name: 'email',
+                placeholder: 'Email Address'
             },
-            password: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'password',
-                    name: 'password',
-                    placeholder: 'Password'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    minLength: 6,
-                },
-                valid: false,
-                touched: false
+            value: '',
+            validation: {
+                required: true,
+                isEmail: true
             },
-            confirmPassword: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'password',
-                    placeholder:"Confirm Password"
-                },
-                value: '',
-                validation: {
-                    required: false,
-                    minLength: 6
-                },
-                valid: false,
-                touched: false
-            }
+            valid: false,
+            touched: false
+        },
+        password: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'password',
+                name: 'password',
+                placeholder: 'Password'
+            },
+            value: '',
+            validation: {
+                required: true,
+                minLength: 6,
+            },
+            valid: false,
+            touched: false
+        },
+        confirmPassword: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'password',
+                placeholder:"Confirm Password"
+            },
+            value: '',
+            validation: {
+                required: false,
+                minLength: 6
+            },
+            valid: false,
+            touched: false
+            
         },
         authLogin: true
     }
+//    state = {
+//        controls: {
+//            email: {
+//                elementType: 'input',
+//                elementConfig: {
+//                    type: 'email',
+//                    name: 'email',
+//                    placeholder: 'Email Address'
+//                },
+//                value: '',
+//                validation: {
+//                    required: true,
+//                    isEmail: true
+//                },
+//                valid: false,
+//                touched: false
+//            },
+//            password: {
+//                elementType: 'input',
+//                elementConfig: {
+//                    type: 'password',
+//                    name: 'password',
+//                    placeholder: 'Password'
+//                },
+//                value: '',
+//                validation: {
+//                    required: true,
+//                    minLength: 6,
+//                },
+//                valid: false,
+//                touched: false
+//            },
+//            confirmPassword: {
+//                elementType: 'input',
+//                elementConfig: {
+//                    type: 'password',
+//                    placeholder:"Confirm Password"
+//                },
+//                value: '',
+//                validation: {
+//                    required: false,
+//                    minLength: 6
+//                },
+//                valid: false,
+//                touched: false
+//            }
+//        },
+//        authLogin: true
+//    }
+    
     componentDidMount () {
         if ( this.props.authRedirectPath !== '/' ) {
             this.props.onSetAuthRedirectPath();
@@ -95,10 +146,47 @@ class Auth extends Component {
         this.setState( { controls: updatedControls } );
     }
 
+    inputChangeHandler = ( event ) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+
     submitHandler = ( event ) => {
         event.preventDefault();
-        this.props.onAuth( this.state.controls.email.value, this.state.controls.password.value, this.state.authLogin);
+        //this.props.onAuth( this.state.controls.email.value, this.state.controls.password.value, this.state.authLogin);
+        console.log(this.state);
+        
+        //validating user using indicatice package
+        //take the input data from state
+        const data = this.state;
+        const schema = {
+            name: 'required|string',
+            email: 'required|email',
+            password: 'required|string|min:6|confirmed' //confirmed will check for the password confirmation
+        }
+        const messages = {
+            required            : 'This {{ field }} is required.',
+            'email.email'       : 'The email is invalid.',
+            'password.min': 'Password is too short',
+            'password.confirmed': 'The password does not match'
+        }
+        
+        validate( data, schema, messages )
+            .then(()=> {
+            console.log('success')
+            })
+        //    .catch(console.error)
+             .catch(errors => {
+             console.log(errors);
+             //show errors to user
+             const formattedErrors = {}
+             errors.forEach( error => formattedErrors[error.field] = error.message )
+             this.setState({ errors: formattedErrors })
+         })
     }
+
     render () {
         const formElementsArray = [];
         for ( let key in this.state.controls ) {
@@ -107,20 +195,74 @@ class Auth extends Component {
                 config: this.state.controls[key]
             } );
         }
-        let form = formElementsArray.map( formElement => 
-            <Input
-                names={formElement.config.name}
-                type={formElement.config.type}
-                key={formElement.id}
-                elementType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                invalid={!formElement.config.valid}
-                shouldValidate={formElement.config.validation}
-                touched={formElement.config.touched}
-                placeholder={formElement.config.placeholder}
-                className={myClasses.AuthInput}
-                changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
+        // let form = formElementsArray.map( formElement => 
+        //     <Input
+        //         names={formElement.config.name}
+        //         type={formElement.config.type}
+        //         key={formElement.id}
+        //         elementType={formElement.config.elementType}
+        //         elementConfig={formElement.config.elementConfig}
+        //         value={formElement.config.value}
+        //         invalid={!formElement.config.valid}
+        //         shouldValidate={formElement.config.validation}
+        //         touched={formElement.config.touched}
+        //         placeholder={formElement.config.placeholder}
+        //         className={myClasses.AuthInput}
+        //         changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
+        // )
+
+        
+        let form = (
+            <Auxiliary>
+                <input 
+                    type="text"
+                    name="email"    
+                    onChange={(event) => this.inputChangeHandler( event, "email")}
+                    placeholder="Email Address"
+                    className={myClasses.AuthInput}
+                />
+                <input 
+                    type="password"
+                    name="password"
+                    onChange={(event) => this.inputChangeHandler( event, "password")}
+                    placeholder="Password"
+                    className={myClasses.AuthInput}
+                />
+            </Auxiliary>
+        )
+        if(!this.state.authLogin) form = (
+            <Auxiliary>
+                <div className="form-group">
+                    <input 
+                        type="text"
+                        name="email"    
+                        onChange={(event) => this.inputChangeHandler( event, "email")}
+                        placeholder="Email Address"
+                        //className={["form-control", myClasses.AuthInput].join(' ')}
+                        className="form-control"
+                    />                    
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="password"
+                        name="password"
+                        onChange={(event) => this.inputChangeHandler( event, "password")}
+                        placeholder="Password"
+                        //className={["form-control", myClasses.AuthInput].join(' ')}
+                        className="form-control"
+                    />                    
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="password"
+                        name="password_confirmation"
+                        onChange={(event) => this.inputChangeHandler( event, "password")}
+                        placeholder="Confirm Password"
+                        //className={["form-control", myClasses.AuthInput].join(' ')}
+                        className="form-control"
+                    />                    
+                </div>
+            </Auxiliary>
         )
 
         if ( this.props.loading ) {
@@ -179,15 +321,17 @@ class Auth extends Component {
                         </button>   
                     </div>
                     
-                    <form action={"/auth/" + act} 
-                        method="post"
-                        // onSubmit={this.submitHandler}
+                <form 
+                    className="form-type-material"
+                    action={"/auth/" + act} 
+                    method="post"
+                        onSubmit={this.submitHandler}
                     >                    
                         {form}
                         <p className="text-left">Forgot Password?</p>
             
                     <button 
-                        //onClick={this.loginHandler} 
+                        onClick={this.loginHandler} 
                         className={[myClasses.Btn, myClasses.AuthBtn, 'auth-btn' ].join(' ')}
                         type="submit"
                     >
