3c7058f7281a41904c52d48c94f5d8ec4b273d1e
Uriel Zacarias <urielzacarias@gmail.com>
2022-12-09 16:52:29 -0800

    updated server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 const express        = require('express')
 const app            = express()
 const dotenv	       = require('dotenv').config({path:__dirname+'/config/config.env'})
-const PORT           = process.env.PORT || 5000;
+const PORT           = process.env.PORT3 || 5000;
 //console.log('PORT = '+ PORT)
 //const morgan         = require('morgan');
 //const helmet         = require('helmet');
